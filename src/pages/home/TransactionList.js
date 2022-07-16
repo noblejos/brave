1e1e8bfebf92f0eeb52628dac795420b03ef9085
noblejos <98827185+noblejos@users.noreply.github.com>
2022-07-16 04:17:33 +0100

    delete key

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -11,7 +11,7 @@ export default function TransactionList({transactions}) {
             <li key={transaction.id}>
                 <p className={styles.name}>{transaction.name}</p>
                 <p className={styles.amount}>${transaction.amount}</p>
-                <button onClick={()=> deleteDocument(transaction.id)}>#</button>
+                <button onClick={()=> deleteDocument(transaction.id)}>*</button>
             </li>
         ))}
     </ul>
